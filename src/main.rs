1e5db94c56ccf045bc77fa32db66086d5e1034b7
Simon Walker <s.r.walker101@googlemail.com>
2020-03-01 00:20:24 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
+use warp::Filter;
+
+struct Controller {
+    rx: UnboundedReceiver<()>,
+}
+
+impl Controller {
+    async fn event_loop(&mut self) {
+        while let Some(msg) = self.rx.recv().await {
+            println!("{:?}", msg);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = unbounded_channel();
+
+    let mut controller = Controller { rx };
+
+    tokio::spawn(async move {
+        controller.event_loop().await;
+    });
+
+    let hello = warp::path!("hello" / String).map(|name| {
+        let tx = tx.clone();
+        async move {
+            tx.send(()).await.unwrap();
+        };
+
+        format!("Hello {}", name)
+    });
+
+    warp::serve(hello).run(([127, 0, 0, 1], 8080)).await;
 }
