18846ec3f8ef746776ea778df881075c0dae88ab
Simon Walker <s.r.walker101@googlemail.com>
2020-03-02 21:10:20 +0000

    Add routes to handler

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
 use warp::Filter;
 
+type Message = ();
+
 struct Controller {
-    rx: UnboundedReceiver<()>,
+    rx: UnboundedReceiver<Message>,
 }
 
 impl Controller {
@@ -10,6 +12,65 @@ impl Controller {
         while let Some(msg) = self.rx.recv().await {
             println!("{:?}", msg);
         }
+    }
+}
+
+mod routes {
+    use crate::handlers;
+    use crate::Message;
+    use tokio::sync::mpsc::UnboundedSender;
+    use warp::Filter;
+
+    pub(crate) fn build(
+        tx: UnboundedSender<()>,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        trigger(tx.clone()).or(webhook(tx.clone()))
+    }
+
+    /// POST /api/trigger
+    pub(crate) fn trigger(
+        tx: UnboundedSender<Message>,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("trigger")
+            .and(warp::post())
+            .and(with_inbox(tx))
+            .and_then(handlers::handle_trigger)
+    }
+
+    /// POST /api/webhook
+    pub(crate) fn webhook(
+        tx: UnboundedSender<Message>,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("webhook")
+            .and(warp::post())
+            .and(with_inbox(tx))
+            .and_then(handlers::handle_webhook)
+    }
+
+    fn with_inbox(
+        tx: UnboundedSender<Message>,
+    ) -> impl Filter<Extract = (UnboundedSender<Message>,), Error = std::convert::Infallible> + Clone
+    {
+        warp::any().map(move || tx.clone())
+    }
+}
+
+mod handlers {
+    use crate::Message;
+    use std::convert::Infallible;
+    use tokio::sync::mpsc::UnboundedSender;
+    use warp::http::StatusCode;
+
+    pub(crate) async fn handle_trigger(
+        _tx: UnboundedSender<Message>,
+    ) -> Result<impl warp::Reply, Infallible> {
+        Ok(StatusCode::NO_CONTENT)
+    }
+
+    pub(crate) async fn handle_webhook(
+        _tx: UnboundedSender<Message>,
+    ) -> Result<impl warp::Reply, Infallible> {
+        Ok(StatusCode::NO_CONTENT)
     }
 }
 
@@ -23,14 +84,14 @@ async fn main() {
         controller.event_loop().await;
     });
 
-    let hello = warp::path!("hello" / String).map(|name| {
-        let tx = tx.clone();
-        async move {
-            tx.send(()).await.unwrap();
-        };
+    let api = routes::build(tx);
+    let routes = api.with(warp::log("dockerdeploy"));
 
-        format!("Hello {}", name)
-    });
+    //     let hello = warp::path!("hello" / String).map(|name| async move {
+    //         tx.send(()).await.unwrap();
 
-    warp::serve(hello).run(([127, 0, 0, 1], 8080)).await;
+    //         format!("Hello {}", name)
+    //     });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
